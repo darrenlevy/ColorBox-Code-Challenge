6b522310d6d532d79f85a4c79d24b6b272464008
Darren <gurudarren@gmail.com>
2011-06-08 12:44:51 -0400

    Solution

---
+++ javascript/custom.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+	$("a[rel='photo']").colorbox({transition:"fade", innerWidth:400, innerHeight:400});
+	$(".youtube").colorbox({iframe:true, innerWidth:425, innerHeight:344});
+});
