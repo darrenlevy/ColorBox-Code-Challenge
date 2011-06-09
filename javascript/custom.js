341053927c70c97db861abd36bc871d6c2dbfd9f
Darren <gurudarren@gmail.com>
2011-06-09 14:57:46 -0400

    Solution updated

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-	$("a[rel='photo']").colorbox({transition:"fade", innerWidth:400, innerHeight:400});
+	$("a[rel='photo']").colorbox({slideshow:true, transition:"fade", innerWidth:400, innerHeight:400});
 	$(".youtube").colorbox({iframe:true, innerWidth:425, innerHeight:344});
 });
